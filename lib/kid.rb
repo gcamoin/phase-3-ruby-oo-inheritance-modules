579f88bfee66d9aa35c3f0de9bfe052fc045ec08
Garrett <gcamoin5@gmail.com>
2023-09-28 16:06:19 -0400

    done

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,15 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative '/home/gcamoin/phase-3-ruby-oo-inheritance-modules/lib/fancy_dance.rb'
+
+class Kid
+    extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+attr_accessor :name
+
+def initialize(name)
+    @name = name
+end
+
+end
